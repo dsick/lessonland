2026d18d371aeef727ae2de919447fdf14cfb61a
dsick <sick@davesick.net>
2016-06-16 16:20:14 -0700

    rails new and gems added. getting error on devise facebook connectable

---
+++ config/environment.rb
@@ -0,0 +1,9 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+config.gem 'devise'
+config.gem 'devise_facebook_connectable'
+config.gem 'facebooker'
+# Initialize the Rails application.
+Rails.application.initialize!
+
+
