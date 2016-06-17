5986df2aaa963e6582cb71113968494f7f5faf13
dsick <sick@davesick.net>
2016-06-16 17:35:22 -0700

    finished initial app

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-config.gem 'devise'
-config.gem 'devise_facebook_connectable'
-config.gem 'facebooker'
+
 # Initialize the Rails application.
 Rails.application.initialize!
 
