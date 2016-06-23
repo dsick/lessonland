13b46bc0e08b858c5c3d9487f6cdb1dc602859e2
dsick <sick@davesick.net>
2016-06-23 11:31:02 -0500

    got initial edit  profile working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622203449) do
+ActiveRecord::Schema.define(version: 20160622212300) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160622203449) do
     t.string   "profile_photo"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "description"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
