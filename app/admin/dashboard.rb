fd8afe67f1b0c6c19287724f6e844146600d3590
dsick <sick@davesick.net>
2016-06-30 09:35:59 -0700

    final adjustments to Dashboard content

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -5,29 +5,28 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc{ I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+        p "Welcome to Lesson Land admin. Please select an item from the menu above to add, remove or modify lessons."
       end
     end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
-end
+    columns do
+      column do
+        panel "Recent Lessons" do
+          ul do
+            Lesson.order("created_at DESC").limit(6).map do |lesson|
+              li link_to(lesson.name, admin_lesson_path(lesson))
+            end
+          end
+        end
+      end
+    end
+      #   column do
+      #     panel "Info" do
+      #       para "Welcome to ActiveAdmin."
+      #     end
+      #   end
+      # end
+    end # content
+  end
